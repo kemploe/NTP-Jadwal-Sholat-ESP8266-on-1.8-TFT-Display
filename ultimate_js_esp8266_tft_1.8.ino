56f705b6bc99709e7eee1cc0cb0d6c33a44b1070
kemploe <kemploebanget@gmail.com>
2022-06-27 18:41:00 +0700

    Update ultimate_js_esp8266_tft_1.8.ino

--- ultimate_js_esp8266_tft_1.8.ino
+++ ultimate_js_esp8266_tft_1.8.ino
@@ -95,7 +95,7 @@ void setup()
 
     // reset settings - wipe stored credentials for testing
     // these are stored by the esp library
-    // wfm.resetSettings();
+    wfm.resetSettings();
 
     // Automatically connect using saved credentials,
     // if connection fails, it starts an access point with the specified name ( "AutoConnectAP" ),
